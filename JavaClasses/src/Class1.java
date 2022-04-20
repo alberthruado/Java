15f20e5f1a901ae2b53172d7c59a06c0bece3a78
Alberth Ruado <31713127+alberthruado@users.noreply.github.com>
2022-04-20 10:03:20 +0800

    Initial commit

---
+++ JavaClasses/src/Class1.java
@@ -0,0 +1,5 @@
+
+public class Class1 {
+	int num = 2;
+	char[] chars = {'a','b','c'};
+}
---
+++ JavaClasses/src/Class2.java
@@ -0,0 +1,6 @@
+
+public class Class2 {
+	void hiImClass2() {
+		System.out.print("Hello I'm from Class 2");
+	}
+}
